4901cdaa9469797f7a7346d762cb39cc882bdb81
kekre <kekre.gaurav@gmail.com>
2018-04-16 17:01:07 -0500

    updated the file.

--- algo/src/arraystring/CheckBoard.java
+++ algo/src/arraystring/CheckBoard.java
@@ -8,64 +8,61 @@ public class CheckBoard {
 	public static void main(String[] args) {
 		CheckBoard checkBoard = new CheckBoard();
 		String word = "ABCCED";
-		checkBoard.checkIfExists(getBoard(), word);
+		checkBoard.doesItExists(getBoard(), word);
 		
 
 	}
 	
 	private static char[][] getBoard(){
-		char[][] board = {{'A','B','C','E'},
+		char[][] myBoard = {{'A','B','C','E'},
 						  {'S','F','C','S'}, 
 						  {'A','D','E','E'} 
 						};
-		return board;
+		return myBoard;
 	}
     
-	//Helper method that  prints final output
-	private void checkIfExists(char[][] board, String word) {
-		if (isWordExists(word, constructLists(board, 3, 4))) {
+	
+	private void doesItExists(char[][] board, String word) {
+		if (doesWrdExists(word, createHashTable(board, 3, 4))) {
 			System.out.println("Exists");
 		} else {
 			System.out.println("Does not exists");
 		}
 	}
 	
-	/*Helper method to check if word really exists, this method takes two parameters the word which is string 
-	 * and a HashTable which is getting passed from the method checkIfExists. 
-	 * 
-	 * */
-	private boolean isWordExists(String word, Hashtable<Integer, ArrayList<Position>> hashtable) {
+	
+	private boolean doesWrdExists(String word, Hashtable<Integer, ArrayList<PosXY>> hashtable) {
 		if(word == null){
 			return true;
 		}
-		ArrayList<Position> list = hashtable.get(word.charAt(0)-'A');
+		ArrayList<PosXY> list = hashtable.get(word.charAt(0)-'A');
 		if(list == null){
 			return false;
 		}
-		for (Position position : list) {
-			position.traversed = true;
-			if(isSubWordExists(word.substring(1),hashtable,position)){
+		for (PosXY position : list) {
+			position.visited = true;
+			if(isSubStrOfWrdExists(word.substring(1),hashtable,position)){
 				return true;
 			}
-			position.traversed = false;
+			position.visited = false;
 		}
 		return false;
 	}
 	
 	//This method calls itself recursively for each substring of the word to check if it exists in the matrix. 
 
-	private boolean isSubWordExists(String substring, Hashtable<Integer, ArrayList<Position>> hashtable,
-			Position position) {
+	private boolean isSubStrOfWrdExists(String substring, Hashtable<Integer, ArrayList<PosXY>> hashtable,
+			PosXY position) {
 		if(substring.equals("")){
 			return true;
 		}
-		ArrayList<Position> list = hashtable.get(substring.charAt(0)-'A');
+		ArrayList<PosXY> list = hashtable.get(substring.charAt(0)-'A');
 		if(list == null){
 			return false;
 		}
 		if(substring.length() == 1){
-			for (Position neghPos : list) {
-				if(neghPos.traversed == true){
+			for (PosXY neghPos : list) {
+				if(neghPos.visited == true){
 					continue;
 				}
 				if(position.x == neghPos.x){
@@ -80,46 +77,45 @@ public class CheckBoard {
 			}
 			return false;
 		}else{
-			for (Position neghPos : list) {
-				neghPos.traversed = true;
-				if(isSubWordExists(substring.substring(1), hashtable, neghPos)){
+			for (PosXY neghPos : list) {
+				neghPos.visited = true;
+				if(isSubStrOfWrdExists(substring.substring(1), hashtable, neghPos)){
 					return true;
 				}
-				neghPos.traversed = false;
+				neghPos.visited = false;
 			}
 		}
 		
 		return false;
 	}
 	
-	//This method creates and return a HashTable with key as position which is Integer, and each Integer represents a 
-	//position in the matrix. and the corresponding value for each hashTable key is a List of the Position class. 
-	public Hashtable<Integer, ArrayList<Position>> constructLists(char[][] board, int length, int width){
-		Hashtable<Integer, ArrayList<Position>> occurancePositions = new Hashtable<Integer,ArrayList<Position>>(26);
+	
+	public Hashtable<Integer, ArrayList<PosXY>> createHashTable(char[][] board, int length, int width){
+		Hashtable<Integer, ArrayList<PosXY>> occurancePositions = new Hashtable<Integer,ArrayList<PosXY>>(26);
 		for(int i=0;i<length;i++){
 			for(int j=0;j<width;j++){
-				ArrayList<Position> list = occurancePositions.get(board[i][j] -'A');
+				ArrayList<PosXY> list = occurancePositions.get(board[i][j] -'A');
 				if(list == null){
-					list = new ArrayList<Position>();
+					list = new ArrayList<PosXY>();
 					occurancePositions.put(board[i][j] -'A', list);
 					System.out.println((board[i][j] -'A'));
 				}
-				list.add(new Position(i, j));
+				list.add(new PosXY(i, j));
 			}
 		}
 		return occurancePositions;
 	}
 	
 	//To store the positions of 2D array and to keep track if that position is already traversed or not. 
-	class Position{
+	class PosXY{
 		int x;
 		int y;
-		boolean traversed;
+		boolean visited;
 		
-		public Position(int x,int y) {
+		public PosXY(int x,int y) {
 			this.x = x;
 			this.y = y;
-			traversed = false;
+			visited = false;
 		}
 	}
 
